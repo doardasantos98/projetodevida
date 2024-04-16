bfd40a06195b2388723226202367d86f8160371a
bruninha98 <129185317+doardasantos98@users.noreply.github.com>
2024-04-16 08:40:47 -0300

    jhhhnhn

--- main.js
+++ main.js
@@ -14,9 +14,16 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores= document.querySelectorAll(".contador");
-const tempoObjetivo1= new Date("2024-10-05T00:00:00");
+const tempoObjetivo1= new Date("2024-05-28T00:00:00");
+const tempoObjetivo2 = new Date("2024-05-39T00:00:00");
+const tempoObjetivo3 = new Date("2024-05-30-T00:00:00");
+const tempoObjetivo4 = new Date("2024-05-31-T00:00:00");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+contadores[0].textContent = calculaTempo(tempos[0]);
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
